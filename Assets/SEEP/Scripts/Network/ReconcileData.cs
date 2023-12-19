45abb1a62e26ed4bc9373ab8fdbe1a08aa5f1959
WatchizzzX <watchizx@yandex.ru>
2023-12-19 09:06:35 +0300

    PredictionV2 enabled. Added base move and reconcile data

---
+++ Assets/SEEP/Scripts/Network/ReconcileData.cs
@@ -0,0 +1,26 @@
+using FishNet.Object.Prediction;
+using UnityEngine;
+
+namespace SEEP.Network
+{
+    public class ReconcileData : IReconcileData
+    {
+        public Vector3 Position;
+        public Quaternion Rotation;
+        public Vector3 Velocity;
+        public Vector3 AngularVelocity;
+        public ReconcileData(Vector3 position, Quaternion rotation, Vector3 velocity, Vector3 angularVelocity)
+        {
+            Position = position;
+            Rotation = rotation;
+            Velocity = velocity;
+            AngularVelocity = angularVelocity;
+            _tick = 0;
+        }
+        
+        private uint _tick;
+        public void Dispose() { }
+        public uint GetTick() => _tick;
+        public void SetTick(uint value) => _tick = value;
+    }
+}
